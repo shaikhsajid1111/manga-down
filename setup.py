271d417b18002990c17bffe51b5dcb60f38a6c05
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-10-27 13:03:42 +0530

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as file:
 
 setuptools.setup(
     name = "manga_down",
-    version = "1.0",
+    version = "0.1.0",
     author = "Shaikh Sajid",
     author_email = "shaikhsajid3732@gmail.com",
     description = "Python package to download manga available on mangareader and mangapanda",
